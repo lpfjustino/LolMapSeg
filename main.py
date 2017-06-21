02b1900b647bea210d906ea05560a3ade652cf06
lpfjustino <lpfjustino@gmail.com>
2017-06-21 20:34:30 -0300

    Final version

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import cv2 as cv
+import cv2
+
 import numpy as np
+
+from matplotlib import pyplot as plt
 from fetcher import fetch_champions
 
 import urllib3
@@ -10,85 +13,35 @@ import shutil
 import time
 
 
+
 '''
     Legacy function that used Hough method to identify the champions' portraits
 '''
 def read_map_hough(curr_map, left='False'):
     map_res = [200, 200]
-    map = cv.imread(curr_map, cv.IMREAD_COLOR)[-map_res[0]:,:map_res[1],:]
+    map = cv2.imread(curr_map, cv2.IMREAD_COLOR)[-map_res[0]:,:map_res[1],:]
 
-    gs_map = cv.cvtColor(map, cv.COLOR_BGR2GRAY)
-    gs_map = cv.equalizeHist(gs_map)
+    gs_map = cv2.cvtColor(map, cv2.COLOR_BGR2GRAY)
+    gs_map = cv2.equalizeHist(gs_map)
 
-    circles = cv.HoughCircles(gs_map,cv.HOUGH_GRADIENT,1,20,param1=50,param2=13,minRadius=9,maxRadius=13)
-
-    circles = np.uint16(np.around(circles))
-    for i in circles[0, :]:
-        # draw the outer circle
-        cv.circle(map, (i[0], i[1]), i[2], (0, 255, 0), 2)
-
-    cv.imshow('Map', map)
-    cv.waitKey(0)
-
-    return circles[0,:,:2], map
-
-'''
-    Uses template matching in order to identify the champions' portraits
-'''
-def read_map(curr_map, left='False'):
-    map_res = [200, 200]
-    map = cv.imread(curr_map, cv.IMREAD_COLOR)[-map_res[0]:,:map_res[1],:]
-
-    gs_map = cv.cvtColor(map, cv.COLOR_BGR2GRAY)
-    gs_map = cv.equalizeHist(gs_map)
-
-    circles = cv.HoughCircles(gs_map,cv.HOUGH_GRADIENT,1,20,param1=50,param2=13,minRadius=9,maxRadius=13)
+    circles = cv2.HoughCircles(gs_map,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=13,minRadius=9,maxRadius=13)
 
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         # draw the outer circle
-        cv.circle(map, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        cv2.circle(map, (i[0], i[1]), i[2], (0, 255, 0), 2)
 
-    cv.imshow('Map', map)
-    cv.waitKey(0)
+    cv2.imshow('Map', map)
+    cv2.waitKey(0)
 
     return circles[0,:,:2], map
 
-
-
-def circular_crop_champ(image_name):
-    image = cv.imread(image_name, cv.IMREAD_COLOR)
-    radius = int(image.shape[0]/2)
-
-    for x in range(image.shape[0]):
-        for y in range(image.shape[1]):
-            if (x-radius) ** 2 + (y-radius) ** 2 > radius ** 2:
-                image[x, y, :] = 0
-
-    image = cv.resize(image, (0, 0), fx=1/5, fy=1/5)
-
-    # cv.imshow('ch', image)
-    # cv.waitKey(0)
-
-    return image
-
-def circular_crop_map(candidate):
-    image = candidate
-    radius = int(image.shape[0]/2) - 1 # Ignores 1 px  radius to reduce error from the border
-
-    for x in range(image.shape[0]):
-        for y in range(image.shape[1]):
-            if (x-radius) ** 2 + (y-radius) ** 2 > radius ** 2:
-                image[x, y, :] = 0
-
-    # cv.imshow('ch', image)
-    # cv.waitKey(0)
-
-    return image
-
+'''
+    Legacy function that used Hough circles to find the champions
+'''
 def find_champions(map, circles, all_champs):
-    # cv.imshow('mapa', map)
-    # cv.waitKey(0)
+    # cv2.imshow('mapa', map)
+    # cv2.waitKey(0)
 
     res = 12
     for circle in circles:
@@ -97,19 +50,19 @@ def find_champions(map, circles, all_champs):
         candidate = circular_crop_map(candidate)
         diffs = []
 
-        # cv.imshow('a',candidate)
-        # cv.waitKey(0)
+        # cv2.imshow('a',candidate)
+        # cv2.waitKey(0)
         for i, champ in enumerate(all_champs):
-            # cv.imshow('a',champ['crop'])
-            # cv.waitKey(0)
+            # cv2.imshow('a',champ['crop'])
+            # cv2.waitKey(0)
 
-            gs_champ = cv.cvtColor(champ['crop'], cv.COLOR_BGR2GRAY)
-            gs_candidate = cv.cvtColor(candidate, cv.COLOR_BGR2GRAY)
+            gs_champ = cv2.cvtColor(champ['crop'], cv2.COLOR_BGR2GRAY)
+            gs_candidate = cv2.cvtColor(candidate, cv2.COLOR_BGR2GRAY)
 
-            champ_hist = cv.calcHist([gs_champ],[0],None,[256],[0,256])
-            candidate_hist = cv.calcHist([gs_candidate],[0],None,[256],[0,256])
+            champ_hist = cv2.calcHist([gs_champ],[0],None,[256],[0,256])
+            candidate_hist = cv2.calcHist([gs_candidate],[0],None,[256],[0,256])
 
-            err = cv.compareHist(champ_hist, candidate_hist,cv.HISTCMP_CHISQR)
+            err = cv2.compareHist(champ_hist, candidate_hist,cv2.HISTCMP_CHISQR)
 
             diffs.append(err)
 
@@ -117,9 +70,88 @@ def find_champions(map, circles, all_champs):
         i = np.argmin(diffs)
         print(all_champs[i]['name'])
 
+'''
+    Uses template matching in order to identify the champions' portraits
+'''
+def read_map(curr_map, all_champions, left='False'):
+    map_res = [200, 200]
+    map = cv2.imread(curr_map, cv2.IMREAD_COLOR)[-map_res[0]:,:map_res[1],:]
+
+    # All the 6 methods for comparison in a list
+    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+    # methods = ['cv2.TM_CCOEFF']
+
+    w = 24
+    h = 24
+
+    for meth in methods:
+        for i, champ in enumerate(all_champs):
+            print(champ['name'])
+            template = champ['crop']
+            img = map.copy()
+            method = eval(meth)
+            # Apply template Matching
+            res = cv2.matchTemplate(img, template, method)
+            print(res.shape)
+            print(np.where(res >= 0.8))
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+            if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+                top_left = min_loc
+            else:
+                top_left = max_loc
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+
+            cv2.rectangle(img, top_left, bottom_right, 255, 2)
+
+            plt.subplot(121), plt.imshow(res, cmap='gray')
+            plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+            plt.subplot(122), plt.imshow(img)
+            plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+            plt.suptitle(meth + ' - ' + champ['name'])
+            # plt.show()
+
+            im_name = meth + "_" + champ['name'] + ".png"
+            cv2.imwrite(im_name, img)
+
+
+def circular_crop_champ(image_name):
+    image = cv2.imread(image_name, cv2.IMREAD_COLOR)
+    radius = int(image.shape[0]/2)
+
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+            if (x-radius) ** 2 + (y-radius) ** 2 > radius ** 2:
+                image[x, y, :] = 0
+
+    image = cv2.resize(image, (0, 0), fx=1/5, fy=1/5)
+
+    # cv2.imshow('ch', image)
+    # cv2.waitKey(0)
+
+    return image
+
+def circular_crop_map(candidate):
+    image = candidate
+    radius = int(image.shape[0]/2) # Ignores 1 px  radius to reduce error from the border
+
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+            if (x-radius) ** 2 + (y-radius) ** 2 > radius ** 2:
+                image[x, y, :] = 0
+
+    # cv2.imshow('ch', image)
+    # cv2.waitKey(0)
+
+    return image
+
+
+
 
 def load_champions_images():
     all_champs = fetch_champions()
+    # all_champs = ['Soraka', 'Fiora', 'Ahri', 'Graves', 'Brand', 'Alistar', 'Nautilus']
+    all_champs = ['Alistar']
     base = []
 
     for champ in all_champs:
@@ -129,14 +161,20 @@ def load_champions_images():
     return base
 
 curr_map = 'images/tst.png'
+# curr_map = 'images/img1.png'
 
-circles, map = read_map(curr_map)
+# Hough test
+# circles, map = read_map(curr_map)
+# find_champions(map, circles, all_champs)
+# find_champions(map, circles, all_champs)
 
 all_champs = load_champions_images()
-# find_champions(map, circles, all_champs)
-find_champions(map, circles, all_champs)
+
+read_map(curr_map, all_champs)
+
+# Champion images fetching
 # champions = fetch_champions()
-#images = fetch_images(champions)
+# images = fetch_images(champions)
 
 
 
